d6d28539b6e5b00b02877bc179f1492626ca7aef
Yildiz Yunus <yunusyildiz44800@gmail.com>
2023-12-13 00:40:36 +0100

    Calculatrice fonctionelle manque la multiplication

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 //Data
-let choosenNumbers = [];
-let numbersMemory = [];
+let selectedNumbers = [];
+let memorisedNumbers = 0;
 let choosenOperator = 0;
-let display = 0;
 
 //Number Aggregator
 document.querySelectorAll('.number').forEach(numberButton => {
@@ -10,8 +9,8 @@ document.querySelectorAll('.number').forEach(numberButton => {
 });
 function aggregate(event) {
     //event.target fait référence au DOM qui s'est fait déclenché par le 'click'
-    choosenNumbers.push(event.target.innerText);
-    document.querySelector('.displayer').innerText = choosenNumbers.join('');
+    selectedNumbers.push(event.target.innerText);
+    document.querySelector('.displayer').innerText = selectedNumbers.join('');
 }
 
 //Operator 
@@ -21,21 +20,34 @@ operatorButton.forEach(operator => {
 });
 function operatorSaver(event) {
     choosenOperator = event.target.innerText;
-
-    //numbersMemory.push(choosenNumbers.join(''));
-    document.querySelector('.displayer').innerHTML = numbersMemory;
-    choosenNumbers.length = 0;
-
-    console.log(numbersMemory);
-    console.log(choosenOperator);
+    document.querySelector('.displayer').innerText = choosenOperator;
+    memorisedNumbers = selectedNumbers.join('');
+    selectedNumbers.length = 0;
+    console.log(memorisedNumbers);
 }
 
-//Clear
+//Calculator
+document.querySelector('.egal').addEventListener('click', calculate);
+function calculate () {
+    if (choosenOperator == '+') {
+        let sum = Number(memorisedNumbers) + Number(selectedNumbers.join(''));
+        document.querySelector('.displayer').innerText = sum;
+        console.log(sum)
+    } else if (choosenOperator == '-') {
+        let minus = Number(memorisedNumbers) - Number(selectedNumbers.join(''));
+        document.querySelector('.displayer').innerText = minus;
+    } else if (choosenOperator == '/') {
+        let divide = Number(memorisedNumbers) / Number(selectedNumbers.join(''));
+        document.querySelector('.displayer').innerText = divide;
+    }
+}
+    //Clear
 let clearButton = document.querySelector('.clear');
-clearButton.addEventListener('click', clearChoosenNumbers);
-function clearChoosenNumbers() {
-    choosenNumbers.length = 0;
-    numbersMemory.length = 0;
+clearButton.addEventListener('click', clearselectedNumbers);
+function clearselectedNumbers() {
+    selectedNumbers.length = 0;
+    memorisedNumbers.length = 0;
     choosenOperator.length = 0;
     document.querySelector('.displayer').innerText = 0;
 }
+
